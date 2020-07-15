debf5179f935571845f7d2286dfdb9bd91ad7aba
cyrtstein <steinmetz.curt@gmail.com>
2020-07-15 18:11:42 -0400

    actually getting pretty clean now

--- retro_halftime_covid.py
+++ retro_halftime_covid.py
@@ -33,23 +33,23 @@ def rethaf(dethz, whereami):
 		break
 	return hd
 
+# here is where we read in the data and fill the lists daytz and dethz
 with open('covid_deaths.csv') as covid_csv:
 	# from first line of data slice off first deepee columns - assign the rest to daytz
 	daytz = covid_csv.readline().rstrip("\n").split(',')[deepee:]
-#	print(daytz)
-	# now read the rest of the file one line at a time - 
+	# now read the rest of the file one line at a time - until we get the one we want - then break out
 	for layn in covid_csv.readlines():
 		thingz = layn.rstrip('\n').split(',')
 		if thingz[1] == nashun:
 			# once we get to the line for US, slice off first deepee columns - assign the rest to dethz
 			dethz = thingz[deepee:]
 			break
+
 # assign number of deaths at peak of pandemic to peaky
 peaky = daytz.index(peak)
-# create ordered dictionary with dates and deaths 
-death_table = OrderedDict(zip(daytz, dethz))
-#for dayt in death_table:
-#	print(dayt, death_table[dayt])
+# create ordered dictionary death_table with dates and deaths 
+# and then also create some blank ordered dictionaries for linear regression
+# death_table = OrderedDict(zip(daytz, dethz))
 slopzDict = OrderedDict()
 halfzDict = OrderedDict()
 dethzDict = OrderedDict()
@@ -60,49 +60,31 @@ for eye in range (len(dethz)):
 	
 dayz = list(range(1,len(daytz)+1))
 i_dethz = [ int(deth) for deth in dethz ]
-f_dayz = [ float(dae) for dae in dayz ]
 hookle = rethaf(i_dethz, -1)
-#print(hookle)
 whereizit = 0
 f_halvzies = []
 for eye in range(len(i_dethz)-1, peaky-1, -1):
 	whereizit -= 1
 	dees_one = rethaf(i_dethz, whereizit)
-	print(eye, daytz[eye], dees_one) 
+#	print(eye, daytz[eye], dees_one) 
 	f_halvzies.append(dees_one)
 	halfzDict[daytz[eye]] = dees_one
-###for eech in halfzDict:
-###	print(eech, dethzDict[eech], halfzDict[eech])
 
-#print(i_dethz)
-#print(f_dayz)
 ###   https://realpython.com/linear-regression-in-python/
-print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-backup = -1
 for backup in range(0,70):
 	exx = []
 	why = []
 	dz = []
-#	print("backup = ", backup, "%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-#	backup += 1  
 	for eye, daet in enumerate(daytz[-howmany-backup:len(daytz)-backup]):
-		print("!!!!!!!!!!!!!!!!!!!!!!!", eye, daet, dethzDict[daet], halfzDict[daet])
+#		print("!!!!!!!!!!!!!!!!!!!!!!!", eye, daet, dethzDict[daet], halfzDict[daet])
 		why.append(halfzDict[daet])
 		exx.append(eye)
 		dz.append(daet)
-#x = np.array(f_dayz[-howmany:]).reshape((-1,1))
-#y = np.array(i_dethz[-howmany:])
 	x = np.array(exx).reshape((-1,1))
 	y = np.array(why)
-#print("Here is x:")
-#print(x)
-#print("And here is y:")
-#print(y)
 	model = LinearRegression()
 	model.fit(x,y)
 	r_sq = model.score(x, y)
-#	print('coefficient of determination:', r_sq)
-#	print(backup, dz[0], dz[-1], 'slope:', model.coef_)
 	slopzDict[dz[-1]] = model.coef_
 	intrzDict[dz[-1]] = model.intercept_
 	eeepzDict[dz[-1]] = daytz.index(dz[-1])
